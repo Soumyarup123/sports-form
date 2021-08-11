dd102836200de5907982567549998ee52c2f7d4d
Soumyarup123 <84226112+Soumyarup123@users.noreply.github.com>
2021-08-11 15:14:51 +0530

    Add files via upload

--- results.js
+++ results.js
@@ -10,38 +10,33 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-
-function readResults(year)
-{
-  var results=firebase.database().ref(year);
-  alert(results);
-  results.on('value',(data) => 
-  {
-    var ranks=data.val();
-    var finalranks=ranks.toString().split(',');
-    document.getElementById("result1").innerHTML="<br> 1."+finalranks[1];
-    document.getElementById("result2").innerHTML="<br> 2."+finalranks[2];
-    document.getElementById("result3").innerHTML="<br> 3."+finalranks[3];
-    document.getElementById("result4").innerHTML="<br> 4."+finalranks[4];
-    document.getElementById("result5").innerHTML="<br> 5."+finalranks[5];
-    document.getElementById("result6").innerHTML="<br> 6."+finalranks[6];
-    document.getElementById("result7").innerHTML="<br> 7."+finalranks[7];
-    document.getElementById("result8").innerHTML="<br> 8."+finalranks[8];
-    document.getElementById("result9").innerHTML="<br> 9."+finalranks[9];
-    document.getElementById("result10").innerHTML="<br> 10."+finalranks[10];
-  })
-}
-function submitForm(e)
-{
+document.getElementById('testForm').addEventListener('submit',submitForm);
+function submitForm(e){
   e.preventDefault();
-  var getyear=getInputVal("year");
-  readResults(getyear);
+  var year =getInputVal('year');
+  readResults(year);
 }
-
-function getInputVal(id)
-{
+  function getInputVal(id){
   return document.getElementById(id).value;
 }
 
+function readResults(year){
+  
+  var results=firebase.database().ref(year);
+  results.on('value', (data) => {
+   var ranks = data.val(); 
+   var finalranks=ranks.toString().split(',');
+   document.getElementById("result1").innerHTML ="<br > 1. " +finalranks[1];
+   document.getElementById("result2").innerHTML ="<br > 2. " +finalranks[2];
+   document.getElementById("result3").innerHTML ="<br > 3. " +finalranks[3];
+   document.getElementById("result4").innerHTML ="<br > 4. " +finalranks[4];
+   document.getElementById("result5").innerHTML ="<br > 5. " +finalranks[5];
+   document.getElementById("result6").innerHTML ="<br > 6. " +finalranks[6];
+   document.getElementById("result7").innerHTML ="<br > 7. " +finalranks[7];
+   document.getElementById("result8").innerHTML ="<br > 8. " +finalranks[8];
+   document.getElementById("result9").innerHTML ="<br > 9. " +finalranks[9];
+   document.getElementById("result10").innerHTML ="<br > 10. " +finalranks[10];
 
-document.getElementById('testForm').addEventListener('submit',submitForm);
+  
+})
+}
